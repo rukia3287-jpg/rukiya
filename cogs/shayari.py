6929707c6a66569612d620ea36424d216173140a
rukia3287-jpg <rukia3287@gmail.com>
2025-09-30 13:22:28 +0530

    Update shayari.py

--- cogs/shayari.py
+++ cogs/shayari.py
@@ -51,8 +51,18 @@ class Shayari(commands.Cog):
             "Rishte woh jo dil se bante hain,\nWaqt se nahi, jazbaat se bante hain,\nDosti woh mithaas hai,\nJo zindagi ko khoobsurat banaati hai 🌸",
             
             "Subah ki pehli kiran ho tum,\nRaat ka aakhri taara ho tum,\nZindagi ki har khushi mein,\nMera sabse pyaara sahaara ho tum ✨",
-            
+        
             "Duniya ki reet alag hai,\nDil ki baat alag hai,\nSach mein jeene ka andaaz,\nBas apno ka saath alag hai 💝"
+             "Chaha tha usse dil se, par voh kabhi samjhi nahi,\nHar dafa chuna kisi aur ko, jaise main kahin tha hi nahi.\nMain chahta hoon sirf usko, har pal, har saans mein,\nKash ek baar keh de voh… 'main bhi chahti hoon tujhe, har ek aas mein.'",
+    "Tabah hokar bhi Tabahi nhi Dikhti,\nTabah hokar bhi Tabahi nhi Dikhti,\nYeh Ishq hai Huzoor iski koi Davai nhi Bikti",
+    "Tere saath ki chaah mein khud ko khona pada,\nSach bolna tha jurm, isliye chup rehna pada.\nNa hasi bachi, na aansuon ka dastaan raha,\nTujhse door jaake bhi tera hi hona pada.",
+    "Khamoshi se matlab nahi, Matlab toh baaton ka hai\nDin toh guzar hi jaata hai, Masla to raaton ka hai",
+    "Khuda ke saaye mein socha kab tak sahenge,\nSabar ke daaman ko aur kitna thaamenge?\nDard ne har shabd lafz mein rang bhar diya,\nEk sitara gira, magar kaun tha jo dekhe?\nKhamoshi poochti rahi, jawab kahaan mile?",
+    "Mohabbat na hoti to ghazal kaun likhta,\nKeechad mein khile us phool ko kamal kaun kehta.\nPyaar to kudrat ka karishma hai,\nWarna laash ke ghar ko Taj Mahal kaun kehta",
+    "Akela tha, akela rahe gaya,\nAb aadat hi ban gaya chhupane ki...\nDost banane ka junoon gum ho gaya,\nNa baat ho kisi se, dil udaas ho gaya",
+    "Na main kahani, na koi kirdar,\nBas ek mohra, bas ek intezar.\nJab zaroorat thi tab kiya yaad,\nAb bhool gaye jaise bekaar",
+    "Hum bhi Bazaar mein the, tum bhi bazaar mein the,\nham pe bhi phool baras rahe the, tum pe bhi phool baras rahe the,\nbas farq itna hain, tum doli se aa rahe the, ham janaze se ja rahe the.",
+    "Dil ke jazbaat ko lafzon se kahoon kaise,\npyaar ka izhaar kru toh kru kaise.\nWo samne ho aur chup sa mai rahu,\nIn aankhon ki zuban se usse samjhau kaise"
         ]
         
         # Standard quality shayaris
